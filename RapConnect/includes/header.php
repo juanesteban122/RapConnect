24d0fe25c7574da83a7282930e30ecc0ca5e3d6c
unknown <gjuanesteban122@gmail.com>
2024-07-25 17:18:28 -0500

    Arregle redireccion al inicio (index)

--- RapConnect/includes/header.php
+++ RapConnect/includes/header.php
@@ -1,5 +1,5 @@
 <header>
-    <a href="/RapConnect/index.php"><h1>RapConnect</h1></a>
+    <a href="../index.php"><h1>RapConnect</h1></a>
     <a href="seleccion_formulario.php">Raperos</a>
 
 </header>
