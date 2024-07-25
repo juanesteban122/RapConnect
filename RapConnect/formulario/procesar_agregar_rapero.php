efa7797ad7d8d01eebb0ed3b94d19b05ffa554e7
unknown <gjuanesteban122@gmail.com>
2024-07-25 16:37:08 -0500

    Redonde de las imagenes del index

--- RapConnect/formulario/procesar_agregar_rapero.php
+++ RapConnect/formulario/procesar_agregar_rapero.php
@@ -6,39 +6,41 @@ error_reporting(E_ALL);
 include '../conexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifica si los campos están definidos
     if (isset($_POST['nombre']) && isset($_POST['descripcion']) && isset($_POST['spotify']) && isset($_POST['youtube']) && isset($_FILES['image'])) {
         $nombre = $conn->real_escape_string($_POST['nombre']);
         $descripcion = $conn->real_escape_string($_POST['descripcion']);
         $spotify = $conn->real_escape_string($_POST['spotify']);
         $youtube = $conn->real_escape_string($_POST['youtube']);
-
-        // Manejo de la imagen
+        
+        // Manejo de la carga de archivos
         $image = $_FILES['image'];
         $imageName = $image['name'];
         $imageTmpName = $image['tmp_name'];
         $imageError = $image['error'];
         $imageSize = $image['size'];
-        $imageExt = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
-        $allowedExt = ['jpg', 'jpeg', 'png', 'gif'];
+        $imageDestination = '../images/' . $imageName;
 
-        if (in_array($imageExt, $allowedExt) && $imageError === 0 && $imageSize < 5 * 1024 * 1024) {
-            $newImageName = uniqid('', true) . "." . $imageExt;
-            $imageDestination = '../images/' . $newImageName;
-            move_uploaded_file($imageTmpName, $imageDestination);
+        if ($imageError === 0) {
+            if ($imageSize < 5000000) { // Limita el tamaño de archivo a 5MB
+                move_uploaded_file($imageTmpName, $imageDestination);
 
-            // Inserta en la base de datos
-            $sql = "INSERT INTO raperos (nombre, descripcion, spotify, youtube, image) VALUES ('$nombre', '$descripcion', '$spotify', '$youtube', '$newImageName')";
+                // Inserta en la base de datos
+                $sql = "INSERT INTO raperos (nombre, descripcion, spotify, youtube, image) VALUES ('$nombre', '$descripcion', '$spotify', '$youtube', '$imageName')";
 
-            if ($conn->query($sql) === TRUE) {
-                echo "<script>
-                alert('Se agregó el rapero correctamente');
-                window.location.href = '/RapConnect/index.php';
-                </script>";
+                if ($conn->query($sql) === TRUE) {
+                    echo "<script>
+                    alert('Se agregó el rapero correctamente');
+                    window.location.href = '../index.php';
+                    </script>";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
             } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "El archivo es demasiado grande.";
             }
         } else {
-            echo "Error con la imagen. Asegúrate de que sea una imagen válida y que no supere los 5 MB.";
+            echo "Error al cargar el archivo.";
         }
     } else {
         echo "Todos los campos son obligatorios.";
