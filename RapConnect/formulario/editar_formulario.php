4c6abab86c3dbd354e6b6359682ec37dfed64c6b
unknown <gjuanesteban122@gmail.com>
2024-08-22 18:17:56 -0500

    se cambio que en la id encriptada

--- RapConnect/formulario/editar_formulario.php
+++ RapConnect/formulario/editar_formulario.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,27 +8,39 @@
     <link rel="stylesheet" href="../css/editar_formulario.css">
     <script src="../js/validacion_editar_formulario.js"></script>
 </head>
-
 <body>
     <?php include '../includes/header.php'; ?>
 
     <?php
     include '../conexion.php';
-    $id = $_GET['id'] ?? '';
+
+    // Decodifica el ID en Base64
+    //Esto que la URL quede "encripta" osea antes era editar_formulario.php?id=1 ya es editar_formulario.php?id=NA%3D%3D
+    $id = isset($_GET['id']) ? base64_decode($_GET['id']) : '';
 
     if ($id) {
+        // Prepara la consulta SQL para evitar inyecciones SQL
         $sql = "SELECT * FROM raperos WHERE id = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param('i', $id);
         $stmt->execute();
         $result = $stmt->get_result();
-        $rapero = $result->fetch_assoc();
+        
+        // Verifica si la consulta devolvió un resultado
+        if ($result->num_rows > 0) {
+            $rapero = $result->fetch_assoc();
+        } else {
+            die('No se encontró ningún rapero con el ID especificado.');
+        }
+
         $stmt->close();
     } else {
         die('ID del rapero no especificado.');
     }
+
     $conn->close();
     ?>
+
     <div class="container">
         <h2>Modificar Rapero</h2>
         <form action="procesar_editar_rapero.php" method="POST" enctype="multipart/form-data">
@@ -68,5 +79,4 @@
 
     <?php include '../includes/footer.php'; ?>
 </body>
-
 </html>
